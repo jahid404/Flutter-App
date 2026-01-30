b1758b2ef7c9400c4affabdeda1b989f2146b13b
Jahidul Islam <jsjahidmini@gmail.com>
2026-01-30 19:05:21 +0600

    feat: Redesign app bar with notification and menu buttons, update app title and body text, and add snackbar functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(List<String> args) {
   runApp(MyApp());
 }
 
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Home Activity
 class HomeActivity extends StatelessWidget {
   const HomeActivity({super.key});
 
@@ -22,15 +21,62 @@ class HomeActivity extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Flutter App',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          'Infirmary App',
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
-        backgroundColor: Colors.blueAccent.shade200,
+        titleSpacing: 10,
+        toolbarHeight: 60,
+        backgroundColor: Colors.blue.shade900,
+        foregroundColor: Colors.white,
+        actions: [
+          IconButton(
+            visualDensity: const VisualDensity(
+              horizontal: -4.0,
+              vertical: -4.0,
+            ),
+            onPressed: () => clickNotificationButton(context),
+            icon: const Icon(Icons.notifications, size: 28),
+          ),
+          IconButton(
+            visualDensity: const VisualDensity(
+              horizontal: -4.0,
+              vertical: -4.0,
+            ),
+            onPressed: () => clickMenuButton(context),
+            icon: const Icon(Icons.menu_open_sharp, size: 28),
+          ),
+        ],
       ),
       body: Text(
-        'Hey There!',
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        'Hey, this is body part!',
+        style: TextStyle(
+          color: Colors.blue.shade500,
+          fontSize: 26,
+          fontWeight: FontWeight.bold,
+        ),
       ),
     );
+  }
+
+  // snackbar
+  showSnackbar(BuildContext context, String message) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        showCloseIcon: true,
+        duration: Duration(seconds: 2),
+        behavior: SnackBarBehavior.floating,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        margin: EdgeInsets.all(10),
+      ),
+    );
+  }
+
+  void clickNotificationButton(BuildContext context) {
+    showSnackbar(context, 'Notification button clicked');
+  }
+
+  void clickMenuButton(BuildContext context) {
+    showSnackbar(context, 'Menu button clicked');
   }
 }
